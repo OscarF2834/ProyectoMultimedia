3713e41a0af74ff8f5fbe2c2653a4dd5ef9ce866
CarMurillo <murillocarlos903@gmail.com>
2024-11-07 09:18:18 -0500

    primer commit

---
+++ src/main/java/com/example/mongodb/proyecto/RestauranteParcial3Application.java
@@ -0,0 +1,13 @@
+package com.example.mongodb.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteParcial3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteParcial3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mongodb/proyecto/RestauranteParcial3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mongodb.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteParcial3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
